9df4c9a06f4767115c4bae4079d90d40472c79e9
juyongkim <kimwtgr@gmail.com>
2019-10-29 15:53:20 +0900

    make infra

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.yellowView}>
+        <Text>Hello</Text>
+      </View>
+      <View style={styles.blueView}>
+        <Text>Hello</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  yellowView: {
+    flex:1,
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex: 3,
+    backgroundColor: "blue"
+  }
+});
