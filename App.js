35999d749012b11a301fbba86fd13af1cabb91df
juyongkim <kimwtgr@gmail.com>
2019-10-30 18:18:16 +0900

    complete the weather app

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Alert} from "react-native";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import Weather from "./Weather";
 import * as Location from "expo-location";
 import axios from "axios";
 
-const API_KEY ="27038c5fed204f236dbd98e9cc069655";
+const API_KEY = "27038c5fed204f236dbd98e9cc069655";
 
 export default class extends React.Component {
   state = {
@@ -15,15 +15,18 @@ export default class extends React.Component {
     const {
       data: {
         main: { temp },
+        name,
         weather
       }
     } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
+    
     this.setState({
       isLoading: false,
       condition: weather[0].main,
-      temp
+      temp,
+      name
     });
   };
   getLocation = async () => {
@@ -41,11 +44,11 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading, temp, condition } = this.state;
+    const { isLoading, temp, condition ,name} = this.state;
     return isLoading ? (
       <Loading />
     ) : (
-        <Weather temp={Math.round(temp)} condition={condition} />
+        <Weather temp={Math.round(temp)} condition={condition} name={name} />
       );
   }
 }
--- Weather.js
+++ Weather.js
@@ -7,53 +7,74 @@ import { LinearGradient } from 'expo-linear-gradient';
 const weatherOptions = {
     Haze: {
         iconName: "weather-fog",
-        gradient: ['#948E99','#2E1437']
+        gradient: ['#948E99','#2E1437'],
+        title: "Haze",
+        subtitle: ""
     },
     Thunderstorm: {
         iconName: "weather-lightning-rainy",
-        gradient: ['#141E30','#243B55']
+        gradient: ['#141E30','#243B55'],
+        title: "Thunderstorm",
+        subtitle: ""
     },
     Drizzle:{
         iconName: "weather-rainy",
-        gradient: ['#2C3E50','#FD746C']
+        gradient: ['#2C3E50','#FD746C'],
+        title: "Drizzle",
+        subtitle: ""
     },
     Rain:{
         iconName: "weather-pouring",
-        gradient: ['#2c3e50','#3498db']
+        gradient: ['#2c3e50','#3498db'],
+        title: "Rain",
+        subtitle: ""
     },
     Snow:{
-        iconName: "weather- snowy",
-        gradient: ['#abbaab','#ffffff']
+        iconName: "weather-snowy",
+        gradient: ['#abbaab','#ffffff'],
+        title: "Snow",
+        subtitle: ""
     },
     Atmosphere:{
-        iconName: "weather- hurricane",
-        gradient: ['#FDFC47','#24FE41']
+        iconName: "weather-hurricane",
+        gradient: ['#FDFC47','#24FE41'],
+        title: "Atmosphere",
+        subtitle: ""
     },
     Clear:{
-        iconName: "weather- sunny",
-        gradient: ['#00c6ff','#0072ff']
+        iconName: "weather-sunny",
+        gradient: ['#00c6ff','#0072ff'],
+        title: "Clear",
+        subtitle: ""
     },
     Clouds:{
         iconName: "weather-cloudy",
-        gradient: ['#283048','#859398']
+        gradient: ['#283048','#859398'],
+        title: "Clouds",
+        subtitle: ""
     },
     Mist:{
-        iconName: "weather- fog" ,
-        gradient: ['#215f00','#e4e4d9']
+        iconName: "weather-fog" ,
+        gradient: ['#215f00','#e4e4d9'],
+        title: "Mist",
+        subtitle: ""
     },
     Dust:{
-        iconName: "weather- fog",
-        gradient: ['#403B4A','#E7E9BB']
+        iconName: "weather-fog",
+        gradient: ['#403B4A','#E7E9BB'],
+        title: "Dust",
+        subtitle: ""
     }
 }
 
-export default function Weather({ temp , condition }) {
+export default function Weather({ temp , condition ,name}) {
     return (
             <LinearGradient 
                 colors={weatherOptions[condition].gradient}
                 style={styles.container}>
                 <StatusBar barStyle="light-content" />    
                 <View style={styles.halfContainer}>
+                    <Text style={styles.cityName}>{name}</Text>
                     <MaterialCommunityIcons 
                         size={100} 
                         color="white" 
@@ -61,7 +82,9 @@ export default function Weather({ temp , condition }) {
                     />
                     <Text style={styles.temp}>{temp}℃</Text>
                 </View>
-                <View style={styles.halfContainer}>
+                <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+                    <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                    <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
                 </View>
             </LinearGradient>
     )
@@ -97,5 +120,28 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems:"center"
+    },
+    title:{
+        color:"white",
+        fontSize: 45,
+        fontWeight: "600",
+        marginBottom: 10
+    },
+    subtitle:{
+        fontWeight:"300",
+        color: "white",
+        fontSize: 25
+    },
+    textContainer: {
+        paddingHorizontal:20,
+        alignItems: "flex-start"   
+    },
+    cityName:{
+        color:"white",
+        fontsize: 50,
+        fontWeight: "500",
+        marginBottom: 20
     }
+
+
 })
